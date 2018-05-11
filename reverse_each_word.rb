6129435f56b4e43482063e78c6af4fb8c6507614
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 18:20:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 
 def reverse_each_word(sentence)
   reversed_array = []
-  sentence.split(" ").map do |word|
+  sentence.split(" ").collect do |word|
     reversed_array << word.reverse 
   end 
     reversed_array.join(" ")
