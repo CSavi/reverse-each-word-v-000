dc94331521a98850e308df77cb2f721761d53653
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 18:14:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,9 @@ require 'pry'
 
  def reverse_each_word(sentence)
   reversed_array = []
-  
+  sentence.split(",").each do |word|
+    reversed_array << word.reverse 
+  end 
+  reversed_array
 end  
   
